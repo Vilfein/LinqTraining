3a347f3288accd09f0641a75e19d4a8f4032f47d
Vašek <Vaek@NTB-DOSKAR>
2023-01-25 09:58:54 +0100

    Add project files.

---
+++ LinqTraining/Clovek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTraining
+{
+    public class Clovek
+    {
+        public Clovek(string jmeno, string prijmeni, int vek)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Vek = vek;
+        }
+
+        public string Jmeno { get; private set; }
+        public string Prijmeni { get; private set; }
+
+        public int Vek { get; private set; }
+
+        public bool Zletily { get => Vek > 17; }
+
+
+
+        public override string ToString()
+        {
+            return Jmeno + " " + Prijmeni;
+        }
+    }
+}
---
+++ LinqTraining/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LinqTraining
+{
+    internal class Program
+    {
+
+
+        static List<Clovek> Lidi = new List<Clovek>()
+            {
+                new Clovek("Adam","Sutner",23),
+                new Clovek("Michal","Bílek",24),
+                new Clovek("Petr","Florian",22),
+                new Clovek("Martin","Tobořík",13),
+                new Clovek("Vojtěch","Pokorný",282),
+                new Clovek("Lucie","Pátková",30),
+                new Clovek("Jonáš","Macoun",3),
+                new Clovek("Filip","Malý",42),
+            };
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
